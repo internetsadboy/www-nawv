103d95f99642efb4ad36fd98ce4e5333208588dd
isb <jaredlamont90@gmail.com>
2025-09-05 11:17:36 -0700

    v1: informational layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,102 +1,62 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className=" flex flex-col min-h-screen p-8 pb-10 text-align-center gap-8">
+      <header className="flex  flex-col justify-center gap-4 row-start-1">
+        <h1 className=" text-4xl font-bold">NOT ANOTHER WEDDING VIDEO</h1>
+        <p>Not a checklist type of video</p>
+      </header>
+      <main className=" flex flex-col gap-8">
+        <div>
+          <h3 className=" text-2xl font-semibold">Mixed Media</h3>
+          <ul>
+            <li className="">hi-8</li>
+            <li className="">2013 camera</li>
+            <li className="">35mm film photos</li>
+            <li className="">iphone</li>
+            <li className="">audio</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className=" text-2xl font-semibold">Documentary</h3>
+          <ul>
+            <li className="">Having worked in Film Production in Los Angeles I offer something different</li>
+            <li className="">I'm a documentary filmmaker; <Link className="underline bold" href="https://onewaytix.film" target="_blank">one way tix</Link> (feature-length doc)</li>
+            <li className="">I want to tell your story, not just shoot a checklist of events</li>
+            <li className="">10 years of creating with cameras</li>
+            <li className="">Story structure</li>
+            <li className="">I may ask to do a phone interview with you a week before to use in the edit</li>
+            <li className="">Interview guests, etc</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className=" text-2xl font-semibold">Rates</h3>
+          <h3 className=" text-xl font-semibold">SF City Hall</h3>
+          <ul className=" flex flex-col list-disc list-inside">
+            <li>$1000 for event coverage</li>
+            <li className="">Highlight video (2-5 minutes)</li>
+            <li>50% deposit to book, balance due the day of the event</li>
+          </ul>
+          <h3 className=" text-xl font-semibold">General</h3>
+          <ul className=" flex flex-col list-disc list-inside">
+            <li>$3500 for full day coverage (8 hours)</li>
+            <li className="">Highlight video (3-8 minutes), ceremony, speeches, footage on USB-drive</li>
+            <li>50% deposit to book, balance due the day of the event</li>
+          </ul>
+          <h3 className=" text-xl font-semibold">Budget-Friendly</h3>
+          <ul className=" flex flex-col list-disc list-inside">
+            <li>$1000 for full day coverage (8 hours)</li>
+            <li className="">No edit, just file transfer</li>
+            <li>50% deposit to book, balance due the day of the event</li>
+          </ul>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className=" row-start-3 flex gap-[24px] flex-wrap  justify-center">
+        <div>
+          <h3 className=" text-2xl font-semibold">Footer</h3>
+        </div>
       </footer>
     </div>
   );
